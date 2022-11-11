7c6dabc31ce6434f8fa3ccedc9a76ee657bce283
jagreer7 <89473211+jagreer7@users.noreply.github.com>
2022-11-11 13:41:39 -0700

    Added basic Movement to serial output
    Jackson

--- Maze-Object.ino
+++ Maze-Object.ino
@@ -1,9 +1,27 @@
+const int downPin = 2;
+const int rightPin = 4;
+const int leftPin = 6;
+const int upPin = 8;
+int downVal = 0;
+int rightVal = 0;
+int leftVal = 0;
+int upVal = 0;
+
+char mazeArray[64];
+int timer = 0;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(downPin, INPUT);
+  pinMode(rightPin, INPUT);
+  pinMode(leftPin, INPUT);
+  pinMode(upPin, INPUT);
   
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  downVal = digitalRead(downPin);
+  rightVal = digitalRead(rightPin);
+  leftVal = digitalRead(leftPin);
+  upVal = digitalRead(upPin);
 }
