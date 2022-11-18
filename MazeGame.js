8980e4cf0c683d56be00b8bfd087731e6a1f5560
tranjames267 <58448725+tranjames267@users.noreply.github.com>
2022-11-18 11:01:43 -0700

    Reuploaded

--- MazeGame.js
+++ MazeGame.js
@@ -1,269 +1,157 @@
-const int downPin = 2;
-const int rightPin = 4;
-const int leftPin = 6;
-const int upPin = 8;
-int downVal = 0;
-int rightVal = 0;
-int leftVal = 0;
-int upVal = 0;
+//THIS IS THE FILE WHERE WE ARE TRYING TO GET THE PLAYER POSITION TO MATCH THE NEOPIXEL SYSTEM
+let serial;
 
-struct maze{
-  int mazeNumber;
-  int goal;
-  int mazeArray[64];
-};
+let increment = 50;
+let playerX = increment;
+let playerY = 6*increment;
 
-int timer = 0;
-int timerCounter = 0;
+let movedUp = 0;
+let movedDown = 0;
+let movedLeft = 0;
+let movedRight = 0;
 
-bool downButtonDown = false;
-bool leftButtonDown = false;
-bool rightButtonDown = false;
-bool upButtonDown = false;
-int characterPos = 0;
+let playerPos = 0;
 
-int whichMaze = 0;
-int numGoals = 0;
-bool isOnGoal = false;
-bool isTimeUp = false;
+let whichMaze = 0; // 5 mazes indexed from 0-4
 
-  maze maze1 = {1, 7, {1, 3, 4, 5, 9, 12, 17, 18, 20, 21, 29, 30, 32, 33, 35, 41, 43, 44, 45, 46, 49, 51, 59, 62, 63, 63}};
-  maze maze2 = {2, 56, {0, 1, 2, 6, 12, 17, 18, 19, 20, 21, 22, 23, 29, 32, 33, 37, 41, 42, 43, 51, 52, 53, 54, 54, 54, 54}};
-  maze maze3 = {3, 16, {0, 7, 8, 9, 10, 11, 14, 15, 17, 25, 27, 28, 30, 35, 36, 37, 38, 41, 43, 48, 49, 50, 51, 54, 55, 63}};
-  maze maze4 = {4, 63, {3, 5, 7, 9, 11, 17, 19, 21, 22, 25, 27, 29, 32, 33, 37, 44, 45, 46, 48, 49, 50, 53, 61, 61, 61, 61}};
-  maze maze5 = {5, 0, {1, 9, 10, 12, 15, 18, 20, 22, 23, 28, 33, 34, 35, 36, 37, 38, 41, 46, 51, 54, 56, 57, 58, 59, 61, 62}};
-  maze mazes[5] = {maze1, maze2, maze3, maze4, maze5};
+let numGoals = 0;
+let isOnGoal = 0;
+let isTimeUp = 0;
+let timer = 0;
 
+// Maze Walls
+let mazes = [];
+let wallInfo = [];
 
+// different mazes do not have the same number of walls
+let maze1_x = [];
+let maze1_y = []; 
+let maze2_x = [];
+let maze2_y = []; 
+let maze3_x = [];
+let maze3_y = [];
+let maze4_x = [];
+let maze4_y = [];
+let maze5_x = [];
+let maze5_y = [];
+let maze1 = [];
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(downPin, INPUT);
-  pinMode(rightPin, INPUT);
-  pinMode(leftPin, INPUT);
-  pinMode(upPin, INPUT);
+function setup() {
+ createCanvas(increment*13,increment*9);
+
+ serial = new p5.SerialPort();
+
+ serial.list();
+ serial.open('COM4');
+  //serial.open('COM7');
+
+ serial.on('connected', serverConnected);
+
+ serial.on('list', gotList);
+
+ serial.on('data', gotData);
+
+ serial.on('error', gotError);
+
+ serial.on('open', gotOpen);
+
+ serial.on('close', gotClose);
   
-  Serial.begin(9600);
-  /*
-  for (int i = 1; i <= 64; i++){
-    mazeArray[i] = 'X';
-    if (i % 8 == 0){
-        Serial.println(mazeArray[i]);
-      }
-      else{
-        Serial.print(mazeArray[i]);
-      }
-  }
-  */
-
   
-  /*
-  maze1.mazeNumber = 1;
-maze2.mazeNumber = 2;
-maze3.mazeNumber = 3;
-maze4.mazeNumber = 4;
-maze5.mazeNumber = 5;
-maze1.goal = 7;
-maze2.goal = 56;
-maze3.goal = 16;
-maze4.goal = 63;
-maze5.goal = 0;
-maze1.mazeArray = {1, 3, 4, 5, 9, 12, 17, 18, 20, 21, 29, 30, 32, 33, 35, 41, 43, 44, 45, 46, 49, 51, 59, 62, 63};
-maze2.mazeArray = {0, 1, 2, 6, 12, 17, 18, 19, 20, 21, 22, 23, 29, 32, 33, 37, 41, 42, 43, 51, 52, 53, 54};
-maze3.mazeArray = {0, 7, 8, 9, 10, 11, 14, 15, 17, 25, 27, 28, 30, 35, 36, 37, 38, 41, 43, 48, 49, 50, 51, 54, 55, 63};
-maze4.mazeArray = {3, 5, 7, 9, 11, 17, 19, 21, 22, 25, 27, 29, 32, 33, 37, 44, 45, 46, 48, 49, 50, 53, 61};
-maze5.mazeArray = {1, 9, 10, 12, 15, 18, 20, 22, 23, 28, 33, 34, 35, 36, 37, 38, 41, 46, 51, 54, 56, 57, 58, 59, 61, 62};
-maze mazes[5] = {maze1, maze2, maze3, maze4, maze5};
-*/
+  let i = increment; 
+  
+  maze1 = [1, 3, 4, 5, 9, 12, 17, 18, 20, 21, 29, 30, 32, 33, 35, 41, 43, 44, 45, 46, 49, 51, 59, 62, 63];
+  maze2 = [0, 1, 2, 6, 12, 17, 18, 19, 20, 21, 22, 23, 29, 32, 33, 37, 41, 42, 43, 51, 52, 53, 54];
+maze3 = [0, 7, 8, 9, 10, 11, 14, 15, 17, 25, 27, 28, 30, 35, 36, 37, 38, 41, 43, 48, 49, 50, 51, 54, 55, 63];
+maze4 = [3, 5, 7, 9, 11, 17, 19, 21, 22, 25, 27, 29, 32, 33, 37, 44, 45, 46, 48, 49, 50, 53, 61];
+maze5 = [1, 9, 10, 12, 15, 18, 20, 22, 23, 28, 33, 34, 35, 36, 37, 38, 41, 46, 51, 54, 56, 57, 58, 59, 61, 62];
+  
+  mazes[0] = maze1;
+  mazes[1] = maze2;
+  mazes[2] = maze3;
+  mazes[3] = maze4;
+  mazes[4] = maze5;
 }
 
-bool emptySpaceExists(int position, int walls[]){
-  for(int i = 0; i<26; i++) {
-    if(position == walls[i]){
-      return false; 
-    }
-  }
-return true;   
+function serverConnected() {
+ print("Connected to Server");
 }
 
-void loop() {
-    downVal = digitalRead(downPin);
-    rightVal = digitalRead(rightPin);
-    leftVal = digitalRead(leftPin);
-    upVal = digitalRead(upPin);
+function gotList(thelist) {
+ print("List of Serial Ports:");
+
+ for (let i = 0; i < thelist.length; i++) {
+  print(i + " " + thelist[i]);
+ }
+}
+
+function gotOpen() {
+ print("Serial Port is Open");
+}
+
+function gotClose(){
+ print("Serial Port is Closed");
+ latestData = "Serial Port is Closed";
+}
+
+function gotError(theerror) {
+ print(theerror);
+}
+
+function gotData() {
+  let currentString = serial.readLine();
+
+  //if we have data
+  if (currentString.length > 0) {
+    //split at the comma
+    let gameData = split(currentString, ",");
     
-    if((characterPos != 0 || timer != 0)){
-      timerCounter++;
-      if (timerCounter%60 == 0){
-        timer++;
-      }
-      if (timer == 120){
-        characterPos = 0;
-        whichMaze = 0;
-        timer = 0;
-        numGoals = 0;
-      }
-    }
-      
-      Serial.print(timer); // time
-      Serial.print(',');
-      /*Serial.print(0); // up
-      Serial.print(',');
-      Serial.print(0); // down
-      Serial.print(',');
-      Serial.print(0); // left
-      Serial.print(',');
-      Serial.print(0); // right
-      Serial.print(',');*/
-      Serial.print(characterPos);
-      Serial.print(',');
-      Serial.print(whichMaze);
-      Serial.print(',');
-      Serial.print(numGoals);
-      Serial.print(',');
-      Serial.print(false); // have you reached goal
-      Serial.print(',');
-      Serial.println(mazes[whichMaze].goal);
-      
-      
-  
-      if (characterPos == mazes[whichMaze].goal){
-        numGoals++;
-        Serial.print(timer);
-        Serial.print(',');
-        /*Serial.print(0);
-        Serial.print(',');
-        Serial.print(0);
-        Serial.print(',');
-        Serial.print(0);
-        Serial.print(',');
-        Serial.print(0);
-        Serial.print(',');*/
-        Serial.print(characterPos);
-        Serial.print(',');
-        Serial.print(whichMaze);
-        Serial.print(',');
-        Serial.print(numGoals);
-        Serial.print(',');
-        Serial.print(true);
-        Serial.print(',');
-        Serial.println(mazes[whichMaze].goal);
-        if(whichMaze != 4){
-          whichMaze = whichMaze+1;
-        }
-        else{
-          whichMaze = 0;
-        }
-      }
-      
-      
-      if (upVal == 1 && upButtonDown == false && characterPos - 8 >= 0 && emptySpaceExists(characterPos-8, mazes[whichMaze].mazeArray)){
+    timer = int(gameData[0]);
+    playerPos = int(gameData[1]);
+    whichMaze = int(gameData[2]);
+    numGoals = int(gameData[3]);
+    isOnGoal = int(gameData[4]);
+    isTimeUp = int(gameData[5]);
+    
+  }
+  trim(currentString);
+  if (!currentString) return;
+  latestData = currentString;
+}
 
-        upButtonDown = true;
-        Serial.print(timer);
-        Serial.print(',');
-        /*Serial.print(1);
-        Serial.print(',');
-        Serial.print(0);
-        Serial.print(',');
-        Serial.print(0);
-        Serial.print(',');
-        Serial.print(0);
-        Serial.print(',');*/
-        Serial.print(characterPos);
-        Serial.print(',');
-        Serial.print(whichMaze);
-        Serial.print(',');
-        Serial.print(numGoals);
-        Serial.print(',');
-        Serial.print(true);
-        Serial.print(',');
-        Serial.println(mazes[whichMaze].goal);
-        characterPos = characterPos - 8;
-        
-      }
-      if (upVal == 0 && upButtonDown == true){
-        upButtonDown = false;
-      }  
+function draw() {
+  clear();
+  background("black");
+  fill(255,255,255);
+  strokeWeight = 1;
   
-      if (downVal == 1 && downButtonDown == false && characterPos + 8 <= 63 && emptySpaceExists(characterPos+8, mazes[whichMaze].mazeArray)){
-        downButtonDown = true;
-        Serial.print(timer);
-        Serial.print(',');
-        /*Serial.print(0);
-        Serial.print(',');
-        Serial.print(1);
-        Serial.print(',');
-        Serial.print(0);
-        Serial.print(',');
-        Serial.print(0);
-        Serial.print(',');*/
-        Serial.print(characterPos);
-        Serial.print(',');
-        Serial.print(whichMaze);
-        Serial.print(',');
-        Serial.print(numGoals);
-        Serial.print(',');
-        Serial.print(true);
-        Serial.print(',');
-        Serial.println(mazes[whichMaze].goal);
-        characterPos = characterPos + 8;
-      }
-      if (downVal == 0 && downButtonDown == true){
-        downButtonDown = false;
-      }  
+  textSize(30);
+  text("Time: " + timer, increment*9.5, increment*2);
+  text("Goals: " + numGoals, increment*9.5, increment*4);
+
   
-      if (leftVal == 1 && leftButtonDown == false && characterPos % 8 != 0 && emptySpaceExists(characterPos-1, mazes[whichMaze].mazeArray)){
-        leftButtonDown = true;
-        Serial.print(timer);
-        Serial.print(',');
-        /*Serial.print(0);
-        Serial.print(',');
-        Serial.print(0);
-        Serial.print(',');
-        Serial.print(1);
-        Serial.print(',');
-        Serial.print(0);
-        Serial.print(',');*/
-        Serial.print(characterPos);
-        Serial.print(',');
-        Serial.print(whichMaze);
-        Serial.print(',');
-        Serial.print(numGoals);
-        Serial.print(',');
-        Serial.print(true);
-        Serial.print(',');
-        Serial.println(mazes[whichMaze].goal);
-        characterPos--;
-      }
-      if (leftVal == 0 && leftButtonDown == true){
-        leftButtonDown = false;
-      }  
+  // stroke(0,0,0);
+  noStroke(); 
   
-      if (rightVal == 1 && rightButtonDown == false && characterPos % 8 != 7&& emptySpaceExists(characterPos+1, mazes[whichMaze].mazeArray)){
-        rightButtonDown = true;
-        Serial.print(timer);
-        Serial.print(',');
-        /*Serial.print(0);
-        Serial.print(',');
-        Serial.print(0);
-        Serial.print(',');
-        Serial.print(0);
-        Serial.print(',');
-        Serial.print(1);
-        Serial.print(',');*/
-        Serial.print(characterPos);
-        Serial.print(',');
-        Serial.print(whichMaze);
-        Serial.print(',');
-        Serial.print(numGoals);
-        Serial.print(',');
-        Serial.print(true);
-        Serial.print(',');
-        Serial.println(mazes[whichMaze].goal);
-        characterPos++;
-      }
-      if (rightVal == 0 && rightButtonDown == true){
-        rightButtonDown = false;
-      }  
+  playerX = (playerPos % 8) * increment + increment;
+  playerY = (floor(playerPos / 8)) * increment + increment;
+  
+  rectMode(CENTER);
+  for (x = 1; x < 9; x++){
+    for (y = 1; y < 9; y++){
+      rect(x * increment, y * increment, increment, increment)
+    }
+  }
+  
+  fill(0,0,0);
+  for (let i = 0; i < mazes[whichMaze].length; i ++){
+    rect((mazes[whichMaze][i] % 8)* increment + increment, (floor(mazes[whichMaze][i] / 8)) * increment +increment, increment, increment)
+  }
+  fill("lightgreen");
+  rect((isTimeUp % 8)* increment + increment, (floor(isTimeUp / 8)) * increment +increment, increment, increment)
+  
+  
+  fill("lightblue");
+  ellipse(playerX, playerY, increment-15, increment-15);
+  
 }
