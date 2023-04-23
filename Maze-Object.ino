e31f6f08d64de6c21f852c8558f0738d42a8d725
jagreer7 <89473211+jagreer7@users.noreply.github.com>
2023-04-23 16:01:36 -0600

    Added 6 new mazes and maze Wall Length to struct

--- Maze-Object.ino
+++ Maze-Object.ino
@@ -23,6 +23,7 @@ struct maze{
   int mazeNumber;
   int startPos; 
   int goal;
+  int mazeArrayLen;
   int mazeArray[64];
 };
 
@@ -40,14 +41,20 @@ bool isOnGoal = false;
 bool isTimeUp = false;
 bool switchTime = false;
 
-  maze maze1 = {1, 0, 7, {1, 3, 4, 5, 9, 12, 17, 18, 20, 21, 29, 30, 32, 33, 35, 41, 43, 44, 45, 46, 49, 51, 59, 62, 63, 63}};
-  maze maze2 = {2, 7, 56, {0, 1, 2, 6, 12, 17, 18, 19, 20, 21, 22, 23, 29, 32, 33, 37, 41, 42, 43, 51, 52, 53, 54, 54, 54, 54}};
-  maze maze3 = {3, 56, 16, {0, 7, 8, 9, 10, 11, 14, 15, 17, 25, 27, 28, 30, 35, 36, 37, 38, 41, 43, 48, 49, 50, 51, 54, 55, 63}};
-  maze maze4 = {4, 16, 63, {3, 5, 7, 9, 11, 17, 19, 21, 22, 25, 27, 29, 32, 33, 37, 44, 45, 46, 48, 49, 50, 53, 61, 61, 61, 61}};
-  maze maze5 = {5, 63, 0, {1, 9, 10, 12, 15, 18, 20, 22, 23, 28, 33, 34, 35, 36, 37, 38, 41, 46, 51, 54, 56, 57, 58, 59, 61, 62}};
-  maze mazes[5] = {maze1, maze2, maze3, maze4, maze5};
+  maze maze1 = {1, 0, 7, 26, {1, 3, 4, 5, 9, 12, 17, 18, 20, 21, 29, 30, 32, 33, 35, 41, 43, 44, 45, 46, 49, 51, 59, 62, 63, 63}};
+  maze maze2 = {2, 7, 56, 26, {0, 1, 2, 6, 12, 17, 18, 19, 20, 21, 22, 23, 29, 32, 33, 37, 41, 42, 43, 51, 52, 53, 54, 54, 54, 54}};
+  maze maze3 = {3, 56, 16, 26, {0, 7, 8, 9, 10, 11, 14, 15, 17, 25, 27, 28, 30, 35, 36, 37, 38, 41, 43, 48, 49, 50, 51, 54, 55, 63}};
+  maze maze4 = {4, 16, 63, 26, {3, 5, 7, 9, 11, 17, 19, 21, 22, 25, 27, 29, 32, 33, 37, 44, 45, 46, 48, 49, 50, 53, 61, 61, 61, 61}};
+  maze maze5 = {5, 63, 0, 26, {1, 9, 10, 12, 15, 18, 20, 22, 23, 28, 33, 34, 35, 36, 37, 38, 41, 46, 51, 54, 56, 57, 58, 59, 61, 62}};
+  maze maze6 = {6, 27, 29, 24, {4, 5, 7, 17, 18, 19, 20, 21, 23, 25, 28, 33, 35, 36, 37, 38, 39, 41, 49, 50, 51, 52, 54, 55}};
+  maze maze7 = {7, 58, 4, 23, {3, 9, 12, 13, 17, 18, 21, 23, 26, 27, 29, 34, 37, 38, 40, 42, 43, 45, 51, 57, 59, 61, 63}};
+  maze maze8 = {8, 16, 32, 21, {9, 11, 14, 17, 19, 24, 25, 27, 30, 33, 34, 35, 38, 41, 43, 46, 49, 51, 52, 53, 54}};
+  maze maze9 = {9, 30, 41, 15, {0, 6, 10, 13, 17, 20, 26, 29, 35, 38, 44, 51, 55, 58, 62}};
+  maze maze10 = {10, 8, 59, 11, {16, 17, 18, 20, 21, 22, 25, 29, 42, 43, 44}};
+  maze maze11 = {11, 32, 27, 13, {6, 9, 14, 19, 20, 26, 29, 34, 43, 44, 49, 54, 57}};
+  maze mazes[11] = {maze1, maze2, maze3, maze4, maze5, maze6, maze7, maze8, maze9, maze10, maze11};
 
-int whichMaze = 0;
+int whichMaze = 0; //Always start on maze 1 (0)
 int prevMaze = -1; // just intially choosing a number that isn't a maze number
 
 void setup() {
@@ -64,9 +71,10 @@ void setup() {
   Serial.begin(9600);
 }
 
-bool emptySpaceExists(int position, int walls[]){
-  for(int i = 0; i<26; i++) {
-    if(position == walls[i]){
+bool emptySpaceExists(int position, int whichMaze1){ //Take in the new position, not the current position 
+  int arrLen = mazes[whichMaze1].mazeArrayLen; //Length is stored in the maze struct
+  for(int i = 0; i < arrLen; i++) {
+    if(position == mazes[whichMaze1].mazeArray[i]){ //Make sure the new position isnt a wall
       return false; 
     }
   }
@@ -75,7 +83,7 @@ return true;
 
 void chooseNewMaze(){ // will change the global variable whichMaze into a random maze number
   prevMaze = whichMaze; 
-  int numOfMazes = 5;
+  int numOfMazes = 11;
   int newMaze = random(0,numOfMazes); // first parameter of random() is inclusive, second parameter is not inclusive
   while(newMaze == prevMaze){ //makes sure that players don't play the same maze in a row while randomizing the order
     newMaze = random(0,numOfMazes); 
@@ -220,7 +228,7 @@ void loop() {
       }
       
       
-      if (upVal == 1 && upButtonDown == false && characterPos - 8 >= 0 && emptySpaceExists(characterPos-8, mazes[whichMaze].mazeArray)){
+      if (upVal == 1 && upButtonDown == false && characterPos - 8 >= 0 && emptySpaceExists(characterPos-8, whichMaze)){
 
         upButtonDown = true;
         strip.setPixelColor(characterPos, 0, 0, 0);
@@ -245,7 +253,7 @@ void loop() {
         upButtonDown = false;
       }  
   
-      if (downVal == 1 && downButtonDown == false && characterPos + 8 <= 63 && emptySpaceExists(characterPos+8, mazes[whichMaze].mazeArray)){
+      if (downVal == 1 && downButtonDown == false && characterPos + 8 <= 63 && emptySpaceExists(characterPos+8, whichMaze)){
         downButtonDown = true;
         strip.setPixelColor(characterPos, 0, 0, 0);
         strip.show();
@@ -268,7 +276,7 @@ void loop() {
         downButtonDown = false;
       }  
   
-      if (leftVal == 1 && leftButtonDown == false && characterPos % 8 != 0 && emptySpaceExists(characterPos-1, mazes[whichMaze].mazeArray)){
+      if (leftVal == 1 && leftButtonDown == false && characterPos % 8 != 0 && emptySpaceExists(characterPos-1, whichMaze)){
         leftButtonDown = true;
         strip.setPixelColor(characterPos, 0, 0, 0);
         strip.show();
@@ -291,7 +299,7 @@ void loop() {
         leftButtonDown = false;
       }  
   
-      if (rightVal == 1 && rightButtonDown == false && characterPos % 8 != 7&& emptySpaceExists(characterPos+1, mazes[whichMaze].mazeArray)){
+      if (rightVal == 1 && rightButtonDown == false && characterPos % 8 != 7&& emptySpaceExists(characterPos+1, whichMaze)){
         rightButtonDown = true;
         strip.setPixelColor(characterPos, 0, 0, 0);
         strip.show();
